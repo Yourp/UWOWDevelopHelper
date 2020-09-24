52cba6316d74d23d35384078d426085b1611dbda
ysmart <zen.89-89@mail.ru>
2020-09-25 00:04:20 +0300

    правим ворнинг

--- ui_settingswindow.h
+++ ui_settingswindow.h
@@ -32,9 +32,8 @@ public:
     QStackedWidget *SW_SettingsLayer;
     QWidget *page;
     QGroupBox *groupBox;
-    QWidget *layoutWidget;
+    QWidget *widget;
     QVBoxLayout *verticalLayout;
-    QVBoxLayout *verticalLayout_3;
     QVBoxLayout *verticalLayout_5;
     QLabel *L_HostName_2;
     QLineEdit *LE_HostName;
@@ -57,7 +56,7 @@ public:
     QWidget *page_2;
     QListWidget *LW_SettingsClassesScripts;
     QGroupBox *groupBox_2;
-    QWidget *widget;
+    QWidget *layoutWidget;
     QHBoxLayout *horizontalLayout_2;
     QLineEdit *LE_ClassesScriptsPath;
     QPushButton *PB_FindScriptFile;
@@ -114,39 +113,37 @@ public:
         groupBox = new QGroupBox(page);
         groupBox->setObjectName(QString::fromUtf8("groupBox"));
         groupBox->setGeometry(QRect(0, 0, 211, 311));
-        layoutWidget = new QWidget(groupBox);
-        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
-        layoutWidget->setGeometry(QRect(10, 30, 192, 271));
-        verticalLayout = new QVBoxLayout(layoutWidget);
+        widget = new QWidget(groupBox);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(10, 30, 192, 271));
+        verticalLayout = new QVBoxLayout(widget);
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
         verticalLayout->setContentsMargins(0, 0, 0, 0);
-        verticalLayout_3 = new QVBoxLayout();
-        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
         verticalLayout_5 = new QVBoxLayout();
         verticalLayout_5->setSpacing(0);
         verticalLayout_5->setObjectName(QString::fromUtf8("verticalLayout_5"));
-        L_HostName_2 = new QLabel(layoutWidget);
+        L_HostName_2 = new QLabel(widget);
         L_HostName_2->setObjectName(QString::fromUtf8("L_HostName_2"));
 
         verticalLayout_5->addWidget(L_HostName_2);
 
-        LE_HostName = new QLineEdit(layoutWidget);
+        LE_HostName = new QLineEdit(widget);
         LE_HostName->setObjectName(QString::fromUtf8("LE_HostName"));
 
         verticalLayout_5->addWidget(LE_HostName);
 
 
-        verticalLayout_3->addLayout(verticalLayout_5);
+        verticalLayout->addLayout(verticalLayout_5);
 
         verticalLayout_8 = new QVBoxLayout();
         verticalLayout_8->setSpacing(0);
         verticalLayout_8->setObjectName(QString::fromUtf8("verticalLayout_8"));
-        L_Port_2 = new QLabel(layoutWidget);
+        L_Port_2 = new QLabel(widget);
         L_Port_2->setObjectName(QString::fromUtf8("L_Port_2"));
 
         verticalLayout_8->addWidget(L_Port_2);
 
-        LE_Port = new QLineEdit(layoutWidget);
+        LE_Port = new QLineEdit(widget);
         LE_Port->setObjectName(QString::fromUtf8("LE_Port"));
         LE_Port->setMaximumSize(QSize(40, 16777215));
         LE_Port->setMaxLength(5);
@@ -154,33 +151,33 @@ public:
         verticalLayout_8->addWidget(LE_Port);
 
 
-        verticalLayout_3->addLayout(verticalLayout_8);
+        verticalLayout->addLayout(verticalLayout_8);
 
         verticalLayout_6 = new QVBoxLayout();
         verticalLayout_6->setSpacing(0);
         verticalLayout_6->setObjectName(QString::fromUtf8("verticalLayout_6"));
-        L_UserName_2 = new QLabel(layoutWidget);
+        L_UserName_2 = new QLabel(widget);
         L_UserName_2->setObjectName(QString::fromUtf8("L_UserName_2"));
 
         verticalLayout_6->addWidget(L_UserName_2);
 
-        LE_UserName = new QLineEdit(layoutWidget);
+        LE_UserName = new QLineEdit(widget);
         LE_UserName->setObjectName(QString::fromUtf8("LE_UserName"));
 
         verticalLayout_6->addWidget(LE_UserName);
 
 
-        verticalLayout_3->addLayout(verticalLayout_6);
+        verticalLayout->addLayout(verticalLayout_6);
 
         verticalLayout_7 = new QVBoxLayout();
         verticalLayout_7->setSpacing(0);
         verticalLayout_7->setObjectName(QString::fromUtf8("verticalLayout_7"));
-        L_Password_2 = new QLabel(layoutWidget);
+        L_Password_2 = new QLabel(widget);
         L_Password_2->setObjectName(QString::fromUtf8("L_Password_2"));
 
         verticalLayout_7->addWidget(L_Password_2);
 
-        LE_Password = new QLineEdit(layoutWidget);
+        LE_Password = new QLineEdit(widget);
         LE_Password->setObjectName(QString::fromUtf8("LE_Password"));
         LE_Password->setFrame(true);
         LE_Password->setEchoMode(QLineEdit::Password);
@@ -188,34 +185,31 @@ public:
         verticalLayout_7->addWidget(LE_Password);
 
 
-        verticalLayout_3->addLayout(verticalLayout_7);
+        verticalLayout->addLayout(verticalLayout_7);
 
         verticalLayout_9 = new QVBoxLayout();
         verticalLayout_9->setSpacing(0);
         verticalLayout_9->setObjectName(QString::fromUtf8("verticalLayout_9"));
-        L_DatabaseName_2 = new QLabel(layoutWidget);
+        L_DatabaseName_2 = new QLabel(widget);
         L_DatabaseName_2->setObjectName(QString::fromUtf8("L_DatabaseName_2"));
 
         verticalLayout_9->addWidget(L_DatabaseName_2);
 
-        LE_DatabaseName = new QLineEdit(layoutWidget);
+        LE_DatabaseName = new QLineEdit(widget);
         LE_DatabaseName->setObjectName(QString::fromUtf8("LE_DatabaseName"));
 
         verticalLayout_9->addWidget(LE_DatabaseName);
 
 
-        verticalLayout_3->addLayout(verticalLayout_9);
+        verticalLayout->addLayout(verticalLayout_9);
 
-
-        verticalLayout->addLayout(verticalLayout_3);
-
-        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+        verticalSpacer = new QSpacerItem(184, 83, QSizePolicy::Minimum, QSizePolicy::Expanding);
 
         verticalLayout->addItem(verticalSpacer);
 
         horizontalLayout = new QHBoxLayout();
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        PB_Connect = new QPushButton(layoutWidget);
+        PB_Connect = new QPushButton(widget);
         PB_Connect->setObjectName(QString::fromUtf8("PB_Connect"));
         PB_Connect->setEnabled(true);
         PB_Connect->setMinimumSize(QSize(91, 31));
@@ -229,7 +223,7 @@ public:
 
         horizontalLayout->addWidget(PB_Connect);
 
-        PB_Disconnect = new QPushButton(layoutWidget);
+        PB_Disconnect = new QPushButton(widget);
         PB_Disconnect->setObjectName(QString::fromUtf8("PB_Disconnect"));
         PB_Disconnect->setEnabled(false);
         PB_Disconnect->setMinimumSize(QSize(91, 31));
@@ -282,13 +276,13 @@ public:
         groupBox_2 = new QGroupBox(page_2);
         groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
         groupBox_2->setGeometry(QRect(180, 10, 401, 61));
-        widget = new QWidget(groupBox_2);
-        widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(10, 20, 381, 26));
-        horizontalLayout_2 = new QHBoxLayout(widget);
+        layoutWidget = new QWidget(groupBox_2);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(10, 20, 381, 26));
+        horizontalLayout_2 = new QHBoxLayout(layoutWidget);
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
         horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
-        LE_ClassesScriptsPath = new QLineEdit(widget);
+        LE_ClassesScriptsPath = new QLineEdit(layoutWidget);
         LE_ClassesScriptsPath->setObjectName(QString::fromUtf8("LE_ClassesScriptsPath"));
         LE_ClassesScriptsPath->setMouseTracking(true);
         LE_ClassesScriptsPath->setFocusPolicy(Qt::StrongFocus);
@@ -298,7 +292,7 @@ public:
 
         horizontalLayout_2->addWidget(LE_ClassesScriptsPath);
 
-        PB_FindScriptFile = new QPushButton(widget);
+        PB_FindScriptFile = new QPushButton(layoutWidget);
         PB_FindScriptFile->setObjectName(QString::fromUtf8("PB_FindScriptFile"));
         PB_FindScriptFile->setMaximumSize(QSize(25, 16777215));
         QFont font2;
@@ -315,7 +309,7 @@ public:
 
         retranslateUi(SettingsWindow);
 
-        SW_SettingsLayer->setCurrentIndex(1);
+        SW_SettingsLayer->setCurrentIndex(0);
 
 
         QMetaObject::connectSlotsByName(SettingsWindow);
